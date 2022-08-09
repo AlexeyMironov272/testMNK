349d14c0e0068e4d823abff69472e650496a78d0
Alexey Mironov <mironovalexey12@gmail.com>
2022-08-09 21:54:38 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,13 +1,11 @@
-ALTER TABLE deposit
-ADD final_price money;
 
-SELECT data.main_part_number, data.manufacturer, data.category, data.origin, price.price, deposit.deposit, deposit.final_price, quantity.quantity, quantity.warehouse
+SELECT data.main_part_number, data.manufacturer, data.category, data.origin, price.price, deposit.deposit, quantity.quantity, quantity.warehouse
 
-FROM (((data
+FROM ((((data
 INNER JOIN price ON data.part_number = price.part_number)
-INNER JOIN deposit ON price.part_number = deposit.part_number)
-INNER JOIN quantity ON deposit.part_number = quantity.part_number)
-
+INNER JOIN deposit ON data.part_number = deposit.part_number)
+INNER JOIN quantity ON data.part_number = quantity.part_number)
+INNER JOIN weight ON data.part_number = weight.part_number)
 
 
 WHERE warehouse LIKE '3'
